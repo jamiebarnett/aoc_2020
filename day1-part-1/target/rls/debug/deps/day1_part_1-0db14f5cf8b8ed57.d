badf3d358917fcc19451a2f575377f065279ef67
jamiebarnett <jamie.barnett@coull.com>
2020-11-30 16:36:09 +0000

    added gitignore

---
+++ day1-part-1/target/rls/debug/deps/day1_part_1-0db14f5cf8b8ed57.d
@@ -0,0 +1,5 @@
+/Users/jamiebarnett/dev/aoc_2020/day1-part-1/target/rls/debug/deps/day1_part_1-0db14f5cf8b8ed57.rmeta: src/main.rs
+
+/Users/jamiebarnett/dev/aoc_2020/day1-part-1/target/rls/debug/deps/day1_part_1-0db14f5cf8b8ed57.d: src/main.rs
+
+src/main.rs:
---
+++ day1-part-1/target/rls/debug/deps/day1_part_1-b1c609d5b3830ac5.d
@@ -0,0 +1,5 @@
+/Users/jamiebarnett/dev/aoc_2020/day1-part-1/target/rls/debug/deps/day1_part_1-b1c609d5b3830ac5.rmeta: src/main.rs
+
+/Users/jamiebarnett/dev/aoc_2020/day1-part-1/target/rls/debug/deps/day1_part_1-b1c609d5b3830ac5.d: src/main.rs
+
+src/main.rs:
