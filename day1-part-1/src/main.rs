8b2fc9f35fa5c5d071ffa292f5167bd8ca068784
jamiebarnett <jamie.barnett@coull.com>
2020-12-01 10:20:33 +0000

    part 1 done

--- day1-part-1/src/main.rs
+++ day1-part-1/src/main.rs
@@ -1,3 +1,30 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+
 fn main() {
-    println!("Hello, world!");
+    
+    let mut file = match File::open("input.txt") {
+        Ok(f) => f,
+        Err(err) => panic!("{}", err)
+    };
+    
+    let mut input = Vec::new();
+    let mut lines = io::BufReader::new(file).lines();
+    for line in lines {
+            if let Ok(line) = line {
+                let i : isize = line.parse().expect("could not parse input");
+                println!("{}", i);
+                input.push(i);
+            }
+    }
+
+    for i in input.clone() {
+        for j in &input {
+            if i + j == 2020 {
+                println!("{} + {} = 2020", i, j);
+                println!("{} * {} = {}", i, j, i * j)
+            }
+        }
+    }
+
 }
