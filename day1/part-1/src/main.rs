795cd2e525405d737519b06acfcd767aabe7ea2e
jamiebarnett <jamie.barnett@coull.com>
2020-12-02 20:33:49 +0000

    remove unnucessary mut from day 1

--- day1/part-1/src/main.rs
+++ day1/part-1/src/main.rs
@@ -3,13 +3,13 @@ use std::io::{self, BufRead};
 
 fn main() {
     
-    let mut file = match File::open("../input.txt") {
+    let file = match File::open("../input.txt") {
         Ok(f) => f,
         Err(err) => panic!("{}", err)
     };
     
     let mut input = Vec::new();
-    let mut lines = io::BufReader::new(file).lines();
+    let lines = io::BufReader::new(file).lines();
     for line in lines {
             if let Ok(line) = line {
                 let i : isize = line.parse().expect("could not parse input");
